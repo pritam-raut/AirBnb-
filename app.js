d935d21caa55b37a4a8d526952350605e2275952
Pritam Raut <pritamraut2005@gmail.com>
2025-07-09 22:34:47 +0530

    adding show route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 main().then(() => {
     console.log("Connected to MongoDB");
@@ -22,11 +23,20 @@ app.get("/", (req ,res) => {
     res.send("lets go");
 });
 
+//index route
+
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
 });
 
+// read route
+
+app.get("/listings/:id", async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
 // app.get("/test", async (req, res) => {
 //     let sempleListing = new Listing({
 //         tittle: "Sample Listing",
